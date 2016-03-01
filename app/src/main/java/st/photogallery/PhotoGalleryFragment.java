983cff8fc5f3f11e42c0ad1c24ffeba7286e4a3e
Teng Sun <tengsun@cn.ibm.com>
2016-03-01 09:19:01 +0800

    Initial commit

---
+++ app/src/main/java/st/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,33 @@
+package st.photogallery;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PhotoGalleryFragment extends Fragment {
+
+    private GridView gridView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        gridView = (GridView) view.findViewById(R.id.photo_gallery_grid_view);
+        return view;
+    }
+
+}
